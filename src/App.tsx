15da897b7734520aa62d6dec7a374ceb5b3a0195
Germán Martínez <germaga92@gmail.com>
2022-04-26 17:50:12 +0200

    Add app route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useEffect } from 'react'
+import Footer from './sections/Footer'
+import GetInvolved from './sections/GetInvolved'
+import Guardians from './sections/Guardians'
+import Header from './sections/Header'
+import MainSection from './sections/MainSection'
+import PoweredBy from './sections/PoweredBy'
 
 function App() {
-  useEffect(() => {
-    console.log('Welcome to the Safe Guardians Page') //Remove
-  }, [])
-
-  return <div></div>
+  return (
+    <div>
+      <Header />
+      <MainSection />
+      <Guardians />
+      <PoweredBy />
+      <GetInvolved />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
