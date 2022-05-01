1b99b5ec767548bfaeab4a734861522c5870578e
Germán Martínez <germaga92@gmail.com>
2022-05-01 22:39:40 +0200

    Add styled-reset

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+
   * {
     margin: 0;
     padding: 0;
